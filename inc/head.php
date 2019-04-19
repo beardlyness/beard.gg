e28bdb2ad4b17774f94d3b6b2b8cb7881981ad25
Beard <beardlyness@users.noreply.github.com>
2019-04-19 05:45:46 -0700

    Create head.php

---
+++ inc/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <!-- Title -->
+    <title>@Beard: GG</title>
+
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Welcome to Beards personal website!">
+    <meta name="keywords" content="beard, BeardlyOG, beardlyness, cryptoworld, Crypto World, Crypto World Foundation, Kernel.is, xmpp.is, torworld, TorWorld, fastrelay, Fast Relay">
+    <meta name="robots" content="index, follow">
+
+    <!-- Stylesheets -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="images/media/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/media/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/media/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/media/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="images/media/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="images/media/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="images/media/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="images/media/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="images/media/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="images/media/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="images/media/favicon-32x32.png" sizes="32x32" />
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="/js/html5shiv.js"></script>
+      <script src="/js/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
