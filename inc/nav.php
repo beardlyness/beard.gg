c742c5b84decea3e32ad727a808f2371bddb3e86
Beard <beardlyness@users.noreply.github.com>
2019-04-19 05:46:28 -0700

    Create nav.php

---
+++ inc/nav.php
@@ -0,0 +1,25 @@
+<body>
+
+  <label for="hamburger" class="hamburger">Hamburger Nahm Nahm..</label>
+  <input type="checkbox" id="hamburger" role="button">
+    <nav id="menu">
+          <ul>
+            <li><a href="https://tv.beard.gg">TV</a></li>
+            <li class="dropdown">
+              <a href="#">Projects</a>
+              <div class="dropdown-content">
+                <a href="https://cryptoworld.is"><small>Crypto World</small></a>
+                <a href="https://torworld.org"><small>TorWorld</small></a>
+                <a href="https://fastrelay.org"><small>FastRelay</small></a>
+                <a href="https://kernel.is"><small>Kernel Project</small></a>
+                <a href="https://xmpp.is"><small>XMPP.IS</small></a>
+                <a href="https://mumble.is"><small>Mumble.IS</small></a>
+                <a href="https://cipher.cx"><small>Cipher.CX</small></a>
+                <a href="https://themanyhats.club"><small>T.M.H.C.</small></a>
+                <a href="https://github.com/UnitedFederationofGaming"><small>U.F.G.</small></a>
+              </div>
+            </li>
+            <li><a href="about">About</a></li>
+            <li><a href="contact">Contact</a></li>
+          </ul>
+        </nav>
