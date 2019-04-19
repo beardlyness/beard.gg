ca93a1cc193b5aa0a255b3582461c6d50fa6c001
Beard <beardlyness@users.noreply.github.com>
2019-04-19 05:45:24 -0700

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+    require 'inc/head.php';
+    require 'inc/nav.php';
+?>
+
+<!-- row -->
+<div class="row" id="hero">
+  <section>
+    <a href="https://beard.gg"><img class="logo" src="images/icons/logo.jpg" /></a>
+      <h1><b style="color:#2ed573;">beard</b> lyness</h1>
+
+        <h4>Personal Contact Details:</h4>
+          <p>Twitter: <a href="https://twitter.com/beardlyness">@beardlyness</a></p>
+          <p>Wire: <a href="https://app.wire.com/">@beardlyness</a></p>
+          <p>Discord: <a href="https://discord.gg">#beard0001</a></p>
+          <p>Reddit: <a href="https://reddit.com/u/BeardGG">/u/BeardGG</a></p>
+          <p>XMPP: <a href="https://xmpp.is">beard [AT] xmpp [DOT] is</a></p>
+          <p>Mumble Server: <a href="https://mumble.is">mumble.is (Port: 64738)</a>
+          <p>Email: <a href="mailto:neck [AT] beard [DOT] gg">neck [AT] beard [DOT] gg</a></p>
+  </section>
+</div>
+
+<?php
+    require 'inc/foot.php';
+?>
