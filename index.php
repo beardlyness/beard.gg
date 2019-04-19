04d999befc18508b56012e6d0af85b72e6170df2
Beard <beardlyness@users.noreply.github.com>
2019-04-19 05:44:48 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include 'inc/head.php';
+    include 'inc/nav.php';
+?>
+      <!-- row -->
+  <div class="row" id="hero">
+    <section>
+      <a href="https://beard.gg"><img class="logo" src="images/icons/logo.jpg" /></a>
+        <h1><b style="color:#2ed573;">beard</b> lyness</h1>
+
+      <h4>Welcome to my personal website!</h4>
+      <p>I've been helping people and making stuff for the Internet since 06.</p>
+      <p>I dabble in a lot of different catagories of tech, and In my free time I play Video Games. </p>
+      </section>
+  </div>
+
+<?php
+    include 'inc/foot.php';
+?>
