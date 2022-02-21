227a70b255a67a110db16a8ce1e03f68f6e6a2d2
Beard <beardlyness@users.noreply.github.com>
2022-02-21 08:58:27 -0800

    https://beard.gg | Website Template Update B02

--- about.php
+++ about.php
@@ -12,9 +12,9 @@
         <div class="contentBox">
           <p class="pasted">
             <h2>Who am I?</h2>
-            <p>I go by beardlyness or ("beard" | "beardly") and I've been around helping with various types of projects for over 10+ Years now.</p>
-            <p>Most of what I know is self taught from reading books, references and manuals. I've dabbled in System Management, Networking, Progamming, Automation, and AI.</p>
-            <p>I've worked with many Operating Systems (Debian/Ubuntu, RHEL/CentOS/Fedora, OpenBSD/FreeBSD) and in different architectures (amd64/X86_64, i386, ARM)</p>
+            <li class="pbody">I go by beardlyness or ("beard" | "beardly") and I've been around helping with various types of projects for over 10+ Years now.</li>
+            <li class="pbody">Most of what I know is self taught from reading books, references and manuals. I've dabbled in System Management, Networking, Progamming, Automation, and AI.</li>
+            <li class="pbody">I've worked with many Operating Systems (Debian/Ubuntu, RHEL/CentOS/Fedora, OpenBSD/FreeBSD) and in different architectures (amd64/X86_64, i386, ARM)</li>
         </p>
       </div>
     </div>
